b29303818d5e8c172f84697df3256bcc2986222f
William Tice <will@untiedgames.com>
2025-03-13 17:27:34 -0500

    Renamed draw(SpriteBatch) to drawInternal, added SpriteBatch parameter to user-facing draw method

--- core/src/main/java/com/untiedgames/TileBeanEngine/Sprite.java
+++ core/src/main/java/com/untiedgames/TileBeanEngine/Sprite.java
@@ -4,6 +4,8 @@ import java.util.Optional;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
+// Sprite is the main way to display things in TileBeanEngine.
+// It provides both update and draw methods, which can be overridden in derived classes.
 public class Sprite extends Component {
 
 	private Texture texture;
@@ -16,12 +18,14 @@ public class Sprite extends Component {
 		// Override me to do custom logic!
 	}
 
-	public void draw() {
+	public void draw(SpriteBatch spritebatch) {
 		// Override me to do custom drawing!
-		draw(TileBeanEngine.getSpriteBatch());
+		drawInternal(spritebatch);
 	}
 
-	protected void draw(SpriteBatch spritebatch) {
+	// Draws the sprite's graphics.
+	// In a custom draw method, you can call this method to draw the sprite's graphics at any time.
+	protected final void drawInternal(SpriteBatch spritebatch) {
 		Optional<Object2D> opt = TileBeanEngine.world.tryGet(owner);
 		if (opt.isPresent()) {
 			Object2D obj = opt.get();
@@ -29,6 +33,7 @@ public class Sprite extends Component {
 			int h = texture.getHeight();
 			float w_half = (float)w * .5f;
 			float h_half = (float)h * .5f;
+			spritebatch.setColor(obj.r, obj.g, obj.b, obj.a);
 			spritebatch.draw(texture, obj.x - w_half, -obj.y - h_half, w_half, h_half, w, h, obj.scale_x, obj.scale_y, -obj.rotation * 180f / (float)Math.PI, 0, 0, w, h, false, false);
 		}
 	}
